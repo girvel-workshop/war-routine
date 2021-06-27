acbf8702994d87784693b03f80679e1f0d1efd6c
girvel <widauka@ya.ru>
2021-06-27 10:17:06 +0500

    Refactor step #2

--- main.lua
+++ main.lua
@@ -12,21 +12,18 @@ function love.load()
 	bullet_speed = 1000
 
 	mc = {
+		image=soldier_normal,
 		position = vector:new(400, 300),
 		velocity = vector.zero(),
 		fire_source = vector:new(-54, -16),
-		rotation = 0
+		rotation = 0,
+		speed = 250,
+		run_multiplier = 1.5,
+		bullets = 30,
+		bullets_max = 30,
+		stamina = 5,
+		stamina_max = 5
 	}
-	mc_speed = 250
-	mc_run_multiplier = 1.5
-	mc_image = soldier_normal
-	mc_fire_source_x = -54
-	mc_fire_source_y = -16
-	mc_bullets = 30
-	mc_bullets_max = 30
-	mc_bullets_other = 90
-	mc_stamina = 5
-	mc_stamina_max = 5
 
 	bullets = {}
 end
@@ -37,25 +34,25 @@ function love.update(dt)
 	mc.velocity = vector.zero()
 
 	if love.keyboard.isDown("w") then
-		mc.velocity.y = -mc_speed
+		mc.velocity.y = -mc.speed
 	end
 	if love.keyboard.isDown("s") then
-		mc.velocity.y = mc_speed
+		mc.velocity.y = mc.speed
 	end
 	if love.keyboard.isDown("a") then
-		mc.velocity.x = -mc_speed
+		mc.velocity.x = -mc.speed
 	end
 	if love.keyboard.isDown("d") then
-		mc.velocity.x = mc_speed
+		mc.velocity.x = mc.speed
 	end
 
 	if love.keyboard.isDown("lshift") then 
-		if mc_stamina > 0 then 
+		if mc.stamina > 0 then 
 			mc.velocity = mc.velocity * mc_run_multiplier
-			mc_stamina = mc_stamina - dt
+			mc.stamina = mc.stamina - dt
 		end
 	else
-		mc_stamina = mc_stamina + dt
+		mc.stamina = mc.stamina + dt
 	end
 
 	mc.position = mc.position + mc.velocity * dt
@@ -76,32 +73,32 @@ function love.keypressed(key)
 	-- ARM / DISARM
 
 	if key == "q" then
-		if mc_image == soldier_normal then
-			mc_image = soldier_armed
+		if mc.image == soldier_normal then
+			mc.image = soldier_armed
 		else
-			mc_image = soldier_normal
+			mc.image = soldier_normal
 		end
 	end
 
 	if key == 'r' then
-		mc_bullets = math.min(mc_bullets_max, mc_bullets_other)
-		mc_bullets_other = mc_bullets_other - mc_bullets
+		mc.bullets = math.min(mc.bullets_max, mc.bullets_other)
+		mc.bullets_other = mc.bullets_other - mc.bullets
 	end
 end
 
 function love.mousepressed(x, y, button, istouch)
-	if button == 1 and mc_image == soldier_armed and mc_bullets > 0 then
+	if button == 1 and mc.image == soldier_armed and mc.bullets > 0 then
 		table.insert(bullets, {
 			position = mc.position + mc.fire_source:rotated(mc.rotation),
 			r = mc.rotation
 		})
-		mc_bullets = mc_bullets - 1
+		mc.bullets = mc.bullets - 1
 	end
 end
 
 function love.draw()
 	love.graphics.setBackgroundColor(.75, .75, .75)
-	draw_image_centralized(mc_image, mc.position, mc.rotation)
+	draw_image_centralized(mc.image, mc.position, mc.rotation)
 
 	for i, b in ipairs(bullets) do 
 		draw_image_centralized(bullet, b.position, b.r)
