598ddc8f36569bf70b6c01bca3e9f2aacbd20fcb
girvel <widauka@ya.ru>
2021-07-15 23:40:07 +0500

    Extracting EROS: move all love functions into engine

--- game.lua
+++ game.lua
@@ -1,4 +1,4 @@
-function love.load()
+function eros.load()
   mc = game:add(assets.units.characters.soldier)
   mc.follows = game:add(mc.legs)
   mc.legs = mc.follows
@@ -17,53 +17,4 @@ function love.load()
   }
 
   game.camera.follows = mc
-end
-
-function love.update(dt)
-  game.world:update(dt, tiny.rejectAll("drawing_system_flag"))
-
-  local subj = game.controller.controls
-
-  -- MOVEMENT
-
-  subj.legs.velocity = vector:zero()
-
-  if love.keyboard.isDown("w") then -- TODO REFACTOR
-    subj.legs.velocity = vector:left():rotated(subj.rotation) * subj.legs.speed
-    assets.actions.move:order(mc.legs)
-  end
-  if love.keyboard.isDown("s") then
-    subj.legs.velocity = -vector:left():rotated(subj.rotation) * subj.legs.speed
-  end
-  if love.keyboard.isDown("a") then
-    subj.rotation = subj.rotation - subj.rotation_speed * dt
-  end
-  if love.keyboard.isDown("d") then
-    subj.rotation = subj.rotation + subj.rotation_speed * dt
-  end
-
-  if love.keyboard.isDown("lshift") then 
-    if subj.stamina:move(-dt) then
-      subj.legs.velocity = subj.legs.velocity * subj.legs.run_multiplier
-    end
-  else
-    subj.stamina:move(dt)
-  end
-end
-
-function love.keypressed(key)
-  game.controller:use_map("keypressed", key)
-end
-
-function love.mousepressed(x, y, button, istouch)
-  game.controller:use_map("mousepressed", button)
-end
-
-function love.draw()
-  game.camera.gamera:setPosition(game.camera.position:unpack())
-  game.camera.gamera:setAngle(game.camera.follows.rotation - math.pi / 2)
-
-  game.camera.gamera:draw(function(l, t, w, h)
-    game.world:update(0, tiny.requireAll("drawing_system_flag"))
-  end)
 end
--- main.lua
+++ main.lua
@@ -14,50 +14,105 @@ if arg[2] == "test" then
 	result = pcall(function() require("tests.main") end)
 	love.event.quit(result)
 else
-	math.randomseed(os.time())
+  function love.load()
+  	math.randomseed(os.time())
 
-	window_size = vector:new(
-    love.graphics.getWidth(),
-    love.graphics.getHeight()
-  )
+  	window_size = vector:new(
+      love.graphics.getWidth(),
+      love.graphics.getHeight()
+    )
 
-	game = {
-    world = tiny.world(
-      unpack(tk.values(require_all("systems")))
-    ),
-    add = function(self, prototype)
-      result = tk.copy(prototype)
-      tiny.add(self.world, result)
-      return result
-    end,
-    remove = function(self, entity)
-      tiny.remove(self.world, entity)
-    end,
+  	game = {
+      world = tiny.world(
+        unpack(tk.values(require_all("systems")))
+      ),
+      add = function(self, prototype)
+        result = tk.copy(prototype)
+        tiny.add(self.world, result)
+        return result
+      end,
+      remove = function(self, entity)
+        tiny.remove(self.world, entity)
+      end,
 
-    controller = {
-    	maps = {},
-    	use_map = function(self, map, key)
-	      local actions = self.maps[map][key]
-	      if not actions then return end
-	      
-	      for _, action in ipairs(actions) do
-	        if action:order(self.controls) then
-	          return
-	        end
-	      end
-	    end
+      controller = {
+      	maps = {},
+      	use_map = function(self, map, key)
+  	      local actions = self.maps[map][key]
+  	      if not actions then return end
+  	      
+  	      for _, action in ipairs(actions) do
+  	        if action:order(self.controls) then
+  	          return
+  	        end
+  	      end
+  	    end
+      }
     }
-  }
 
-  game.camera = game:add({
-    follows = false,
-    position = vector:zero(),
-    anchor = window_size / 2,
-    gamera = gamera.new(-10000, -10000, 20000, 20000) -- TODO levels
-  })
+    game.camera = game:add({
+      follows = false,
+      position = vector:zero(),
+      anchor = window_size / 2,
+      gamera = gamera.new(-10000, -10000, 20000, 20000) -- TODO levels
+    })
 
-  aspects = require_all("eros.aspects")
-  assets = require_all("assets")
+    aspects = require_all("eros.aspects")
+    assets = require_all("assets")
 
-	require("game")
+    eros = {}
+
+  	require("game")
+
+    if eros.load then eros.load() end
+  end
+
+  function love.update(dt)
+    game.world:update(dt, tiny.rejectAll("drawing_system_flag"))
+
+    local subj = game.controller.controls
+
+    -- MOVEMENT
+
+    subj.legs.velocity = vector:zero()
+
+    if love.keyboard.isDown("w") then -- TODO REFACTOR
+      subj.legs.velocity = vector:left():rotated(subj.rotation) * subj.legs.speed
+      assets.actions.move:order(mc.legs)
+    end
+    if love.keyboard.isDown("s") then
+      subj.legs.velocity = -vector:left():rotated(subj.rotation) * subj.legs.speed
+    end
+    if love.keyboard.isDown("a") then
+      subj.rotation = subj.rotation - subj.rotation_speed * dt
+    end
+    if love.keyboard.isDown("d") then
+      subj.rotation = subj.rotation + subj.rotation_speed * dt
+    end
+
+    if love.keyboard.isDown("lshift") then 
+      if subj.stamina:move(-dt) then
+        subj.legs.velocity = subj.legs.velocity * subj.legs.run_multiplier
+      end
+    else
+      subj.stamina:move(dt)
+    end
+  end
+
+function love.keypressed(key)
+  game.controller:use_map("keypressed", key)
+end
+
+function love.mousepressed(x, y, button, istouch)
+  game.controller:use_map("mousepressed", button)
+end
+
+function love.draw()
+  game.camera.gamera:setPosition(game.camera.position:unpack())
+  game.camera.gamera:setAngle(game.camera.follows.rotation - math.pi / 2)
+
+  game.camera.gamera:draw(function(l, t, w, h)
+    game.world:update(0, tiny.requireAll("drawing_system_flag"))
+  end)
+end
 end
