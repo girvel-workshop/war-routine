604b9d932390664d340f319168907809832192c5
girvel <widauka@ya.ru>
2021-08-02 13:52:51 +0500

    Add documentation decorator

--- eros/libraries/girvel/functional.lua
+++ eros/libraries/girvel/functional.lua
@@ -5,49 +5,96 @@ local lambda = require[[eros.libraries.girvel.lambda]]
 
 local fnl = {}
 
-fnl.pipe = decorator:new(function(_, f)
-	return function(...)
-		return setmetatable({args = {...}}, {
-			__div = function(table, self)
-				return f(table, unpack(self.args))
-			end
-		})
-	end
+fnl.docs = decorator:new(function(self, f, documentation)
+	self[f] = documentation
+	return f
 end)
 
-fnl.filter = fnl.pipe() .. function(t, predicate)
-	if type(predicate) == "string" then
-		predicate = lambda("ix, it -> " .. predicate)
+fnl.docs = fnl.docs{
+	type='decorator',
+	description='assigns some documentation to a value, that later can be get by fnl.docs[<value>]'
+} .. fnl.docs
+
+fnl.pipe = 
+	fnl.docs{
+		type='decorator', 
+		description='makes function `f(t, ...)` a pipe function `t / f(...)`',
+		args={
+
+		}
+	} .. 
+	decorator:new(function(_, f)
+		return function(...)
+			return setmetatable({args = {...}}, {
+				__div = function(table, self)
+					return f(table, unpack(self.args))
+				end
+			})
+		end
+	end)
+
+fnl.filter = 
+	fnl.docs{
+		type='pipe function',
+		description='filters table by ipairs',
+		args={
+			'input sequence',
+			'predicate function(ix, it) or implicit (ix, it) lambda'
+		}
+	} ..
+	fnl.pipe() .. 
+	function(t, predicate)
+		if type(predicate) == "string" then
+			predicate = lambda("ix, it -> " .. predicate)
+		end
+
+		local result = {}
+		for i, v in ipairs(t) do
+			if predicate(i, v) then
+				table.insert(result, v)
+			end
+		end
+		return result
 	end
 
-	local result = {}
-	for i, v in ipairs(t) do
-		if predicate(i, v) then
+fnl.values = 
+	fnl.docs{
+		type='pipe function',
+		returns='sequence of values of table t',
+		args={'table'}
+	} ..
+	fnl.pipe() .. 
+	function(t)
+		result = {}
+		for k, v in pairs(t) do
 			table.insert(result, v)
 		end
+		return result
 	end
-	return result
-end
 
-fnl.values = fnl.pipe() .. function(t)
-	result = {}
-	for k, v in pairs(t) do
-		table.insert(result, v)
-	end
-	return result
-end
-
-function fnl.remove(table_, value)
-	for i, v in ipairs(table_) do
-		if v == value then
-			table.remove(table_, i)
-			return true
+fnl.remove =
+	fnl.docs{
+		type='function',
+		description='removes first value from table',
+		returns='removed value or nil'
+	} ..
+	function(t, value)
+		for i, v in ipairs(t) do
+			if v == value then
+				table.remove(t, i)
+				return value
+			end
 		end
 	end
-	return false
-end
 
 -- fnl.extend = fnl.pipe() .. function(table1, table2) -- TODO
+-- fnl.extend = 
+--   fnl.docs{
+--     type='pipe functions',
+--     returns='first table extended by second'
+--   } ..
+--   fnl.pipe() ..
+--  function(table1, table2)
 function fnl.extend(table1, table2)
 	result = table1 / fnl.copy()
 	for k, v in pairs(table2) do
@@ -57,40 +104,66 @@ function fnl.extend(table1, table2)
 end
 
 fnl.copy = nil
-fnl.copy = fnl.pipe() .. function(t, cache, not_deep)
-	if t == nil then return nil end
+fnl.copy = 
+	fnl.docs{
+		type='pipe function',
+		returns='copy of value',
+		args={
+			'value',
+			'table of already copied tables for reference preserving',
+			'disable recursive copying'
+		}
+	} ..
+	fnl.pipe() .. 
+	function(t, cache, not_deep)
+		if t == nil then return nil end
 
-	if type(t) ~= "table" then return t end
-	if not cache then cache = {} end
-	if cache[t] then return cache[t] end
+		if type(t) ~= "table" then return t end
+		if not cache then cache = {} end
+		if cache[t] then return cache[t] end
 
-	if t.copy ~= nil then
-		return t:copy()
-	end
-
-	local result = {}
-
-	setmetatable(result, getmetatable(t))
-	cache[t] = result
-
-	for k, v in pairs(t) do
-		if not_deep or type(v) ~= "table" then
-			result[k] = v
-		else
-			result[k] = v / fnl.copy(cache)
+		if t.copy ~= nil then
+			return t:copy()
 		end
+
+		local result = {}
+
+		setmetatable(result, getmetatable(t))
+		cache[t] = result
+
+		for k, v in pairs(t) do
+			if not_deep or type(v) ~= "table" then
+				result[k] = v
+			else
+				result[k] = v / fnl.copy(cache)
+			end
+		end
+		return result
 	end
-	return result
-end
 
-fnl.inherit = fnl.pipe() .. function(child, parent)
-	setmetatable(child, parent)
-	parent.__index = parent
-	return child
-end
+fnl.inherit = 
+	fnl.docs{
+		type='pipe function',
+		description='sets metatable & parent\'s __index',
+		returns='child',
+		args={'child', 'parent'}
+	} ..
+	fnl.pipe() .. 
+	function(child, parent)
+		setmetatable(child, parent)
+		parent.__index = parent
+		return child
+	end
 
-fnl.contains = fnl.pipe() .. function(collection, element)
-	return #(collection / fnl.filter[[it == element]]) > 0
-end
+fnl.contains = 
+	fnl.docs{
+		type='pipe function',
+		returns='is value in collection',
+		args={'collection', 'value'}
+	} ..
+	fnl.pipe() .. 
+	function(collection, element)
+		return #(collection / fnl.filter[[it == element]]) > 0
+	end
 
 return fnl
